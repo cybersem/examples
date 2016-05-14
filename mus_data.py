cadc1e11b72a9ce393e202bc2e5c06a17148b7bf
Sem Krivitsky <sem.krivitsky@gmail.com>
2016-05-14 20:23:28 +0300

    Добавлен вебсервер

--- mus_data.py
+++ mus_data.py
@@ -2,16 +2,62 @@
 """Программа для работы с базой данных
 """
 
+import datetime
 import pymongo
 
+logfile = open('mus.log', 'a')
 dbserver = 'mongodb://localhost:27017'
 dbname = 'exdb'
 clname = 'unicorns'
+
+#Подключение к БД:
 client = pymongo.MongoClient(dbserver)
 dbs = client.database_names()
 db = client[dbname]
 cls = db.collection_names(include_system_collections=False)
 cl = db[clname]
+sstate = 'Ok' if cl else 'Error'
+strlog = '%s - Подсоединение к БД --> %s' % (datetime.datetime.now().strftime('%d.%m.%Y %I:%M:%S %P'), sstate)
+print(strlog, file=logfile)
+
+#Добавление группы:
+class Group:
+    def __init__(self):
+        pass
+
+group = Group()    
+group.name = input('Введите название группы >>')
+group.year = input('Введите год создания группы >>')
+group.style = input('Введите музыкальный стиль >>')
+
+group.album = None
+
+input('Название альбома >>')
+input('Год выпуска альбома >>')
+
+"""
+Альбом 1. 1980
+    трек 1
+    трек 2
+    трек 3
+
+Альбом 2. 1983
+    трек 1
+    трек 2
+
+Альбом 3.1988
+    трек 1
+    трек 2
+    трек 3
+
+[{'name': 'al1', 'year': '1980'},
+{'name': 'al2', 'year': '1983'}]
+
+"""
+
+
+#Завершение программы:
+logfile.close()
 
 #test:
 if __name__ == '__main__':
---
+++ webserver/cgi-bin/name.py
@@ -0,0 +1,15 @@
+﻿#!C:/Python34
+
+import cgi
+
+form = cgi.FieldStorage()
+
+print('Content-type: text/html\n')
+print('<title>Result</title>')
+
+if not 'user' in form:
+    print('</br><h1>Name is empty! Try again!</h1>\n<a href="../index.html">Back</a>')
+else:
+    print('</br><h1>Hello, <i>%s</i>!</h1>\n<a href="../index.html">Back</a>' % cgi.escape(form['user'].value))
+
+print('</body>')
